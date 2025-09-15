dbbeeac8fde5db5f8403c603511cb80c8421dd8d
wwpeace <ramazanov200628@gmail.com>
2025-09-15 21:25:22 +0300

    l2.14 +dynamic

--- l2.14/main.go
+++ l2.14/main.go
@@ -5,7 +5,24 @@ import (
 	"time"
 )
 
-func or(channels ...<-chan interface{}) <-chan interface{} {
+func orDynamic(channels ...<-chan interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func(ch chan interface{}) {
+		i := 0
+		for {
+			select {
+			case <-channels[i]:
+				close(ch)
+				return
+			default:
+				i = (i + 1) % len(channels)
+			}
+		}
+	}(ch)
+	return ch
+}
+
+func orRecursive(channels ...<-chan interface{}) <-chan interface{} { // goroutines leak above goroutine that closed first
 	ch := make(chan interface{})
 	go func(ch chan<- interface{}, channels ...<-chan interface{}) {
 		defer close(ch)
@@ -15,7 +32,7 @@ func or(channels ...<-chan interface{}) <-chan interface{} {
 			select {
 			case <-channels[len(channels)-1]:
 				break
-			case <-or(channels[:len(channels)-1]...):
+			case <-orRecursive(channels[:len(channels)-1]...):
 				break
 			}
 		}
@@ -33,11 +50,21 @@ func main() {
 		return c
 	}
 	start := time.Now()
-	<-or(
+	<-orRecursive(
 		sig(2*time.Hour),
 		sig(5*time.Minute),
 		sig(4*time.Second),
 		sig(2*time.Second),
+		sig(5*time.Second),
+	)
+	fmt.Printf("done after %v\n", time.Since(start))
+
+	start = time.Now()
+	<-orDynamic(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(4*time.Second),
+		sig(3*time.Second),
 		sig(5*time.Second),
 	)
 	fmt.Printf("done after %v\n", time.Since(start))
