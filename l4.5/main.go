f905fccf33d4fbba3d7417de4d91fe3b77635dd7
wwpeace <ramazanov200628@gmail.com>
2026-03-29 20:42:40 +0300

    l4.5 pooled slice

--- l4.5/main.go
+++ l4.5/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"sync"
 )
 
 func main() {
@@ -13,9 +14,16 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+var p = sync.Pool{
+	New: func() interface{} {
+		return make([]int, 0, 100000)
+	},
+}
+
 func badSumHandler(w http.ResponseWriter, r *http.Request) {
 	sum := 0
-	values := make([]int, 0, 100000)
+	values := p.Get().([]int)
+	defer p.Put(values[:0])
 	for i := 0; i < 100000; i++ {
 		sum += i
 		values = append(values, i)
